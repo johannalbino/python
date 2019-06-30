fc4e5da8feeb07bfecfa525c84667537d39ab320
Johann Albino <johann.albino.ti@gmail.com>
2019-06-30 12:59:06 -0300

    Validacao projeto

--- validacao de duplicidade/func_python2.py
+++ validacao de duplicidade/func_python2.py
@@ -2,37 +2,38 @@
 #author: Johann Allbino 
 
 
-inventario = open("inventario.txt", "r")
-produto = open("0200_0519.txt", "r")
-arq_saida = open ("inventariodif.txt", "w+")
+produto1 = open("0200ant.txt", "r")
+produto2 = open("0200atual.txt", "r")
+arq_saida = open ("dif0200.txt", "w+")
 
-list_produto = []   #lista para receber cada linha do arquivo de produtos
-list_inventario = [] #lista para receber cada linha do arquivo inventario
-it_produtos = []
-it_inventario = []
-for i in inventario:
-    list_inventario.append(i)
+list_produto1 = []   #lista para receber cada linha dos produtos do registro 0200 anterior
+list_produto2 = [] #lista para receber cada linha dos produtos do registro 0200 atual
 
-for b in produto:
-    list_produto.append(b)
+for i in produto1:
+    list_produto1.append(i)
+#criando a lista de produtos do arqivo produto1
+
+for b in produto2:
+    list_produto2.append(b)
+#criando a lista de produtos do arquivo produto2
 
 lmaior = 0 #para saber qual lista menor para rodar o for
 
-print (len(list_produto), len(list_inventario))
-
-if len(list_produto) < len(list_inventario): 
-    lmaior = len(list_inventario)
+#validação de qual lista é maior para a variavel lmaior receber a quantidade de linhas da lista
+if len(list_produto1) < len(list_produto2): 
+    lmaior = len(list_produto2)
 else: 
-    lmaior = len(list_produto)
+    lmaior = len(list_produto1)
 
 li = []
 
-for b in range(0,len(list_produto)):
-    li.append(list_produto[b].split('|')[2])
+for b in range(0,len(list_produto1)):
+    li.append(list_produto1[b].split('|')[2])
+#Recebendo a lista de códigos internos de produtos do arquivo produto1
 
 for i in range(0, lmaior):
-    lp = list_inventario[i].split('|')[2]
+    lp = list_produto2[i].split('|')[2]
     print (lp)
     if lp not in li:
-        arq_saida.writelines(list_inventario[i])
+        arq_saida.writelines(list_produto2[i])
 #arq_saida.close()
